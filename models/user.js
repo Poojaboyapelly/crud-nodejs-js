d069c0a8cbaf32e50ae8da4ee8ac7d9dba1c5c6d
Poojaboyapelly <pooja.boyapelly@bizacuity.com>
2024-02-06 18:38:00 +0530

    working signup

--- models/user.js
+++ models/user.js
@@ -6,30 +6,31 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        validate: {
-            validator: async function (x) {
+        // validate: {
+        //     validator: async function (x) {
 
-                const user = await models.User.findOne({ username: x });
-                return !user;
-            },
-            message: 'Username already exists'
-        }
+        //         const user = await models.User.findOne({ username: x });
+        //         return !user;
+        //     },
+        //     message: 'Username already exists'
+        // }
     },
     password: {
         type: String,
         required: true,
-        validate: {
-            validator: function (v) {
+        // validate: {
+        //     validator: function (v) {
 
-                return /^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,}$/.test(v);
-            },
-            message: 'Password must contain at least one special character and have a minimum length of 8 characters'
-        }
+        //         return /^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,}$/.test(v);
+        //     },
+        //     message: 'Password must contain at least one special character and have a minimum length of 8 characters'
+        // }
     },
-    // role: {
-    //     type: String,
-    //     enum: ['Admin', 'Normal', 'SuperAdmin']
-    // }
+    role: {
+        type: String,
+        enum: ['Admin', 'Normal', 'SuperAdmin'],
+        default:'Normal'
+    }
 });
 
 userSchema.pre('save', function (next) {
--- routers/users.js
+++ routers/users.js
@@ -1,37 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user');
 
-// router.post('/signup', async(req, res) => {
 
-//     const { username, password } = req.body;
+router.post('/', async (req, res) => {
 
-//     //  if  username already exists
-//     const existingUser = await User.findOne({ username });
-//     if (existingUser) {
-//         return res.status(400).json({ error: 'Username already exists' });
-//     }
-
-//     // Create a new user
-//     const newUser = new User({ username, password });
-//     await newUser.save();
+    try {
     
-    
-//     res.json({ message: 'User signed up successfully' });
-//   });
-  
-  
-// router.post('/login', async (req, res) => {
+      const user = new User({
+        username : req.body.username,
+        password : req.body.password
+      }
+      )
+      console.log(user);
+      await user.save()
+      res.status(201).json({succes:true, user})
 
-//     const { username, password } = req.body;
-//   // Find the user by username and password
-//   const user = await User.findOne({ username, password });
-//   if (user) {
-//       res.json({ message: 'Login successful' });
-//   } else {
-//       res.status(401).json({ error: 'Invalid username or password' });
-//   }
-//   });
-const addUser = require ('../controller/user')
-const express = require('express');
 
-const Router =express.Router();
-Router.post('/',addUser);
-module.exports =Router
+    }
+    catch (err) {
+        res.send('Error' + err)
+    }
+
+});
+
+
+module.exports = router
